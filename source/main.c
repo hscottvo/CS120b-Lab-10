787cf2c115086908b175d8c26db17f4be325a102
hscottvo <hscottvo@gmail.com>
2021-05-23 14:39:40 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -22,23 +22,23 @@ unsigned char lock_state = 0x01;
 int keypad_tick(int state) {
     pad = GetKeypadKey();
     switch(pad) {
-        case '\0': PORTB = 0x00 | (~PINB & 0x80); break;
-        case '1': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '2': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '3': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '4': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '5': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '6': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '7': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '8': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '9': PORTB = 0x40 | (~PINB & 0x80); break;
-        case 'A': PORTB = 0x40 | (~PINB & 0x80); break;
-        case 'B': PORTB = 0x40 | (~PINB & 0x80); break;
-        case 'C': PORTB = 0x40 | (~PINB & 0x80); break;
-        case 'D': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '*': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '0': PORTB = 0x40 | (~PINB & 0x80); break;
-        case '#': PORTB = 0x40 | (~PINB & 0x80); break;
+        case '\0': PORTB = 0x00 | (~PINB & 0x81); break;
+        case '1': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '2': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '3': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '4': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '5': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '6': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '7': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '8': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '9': PORTB = 0x40 | (~PINB & 0x81); break;
+        case 'A': PORTB = 0x40 | (~PINB & 0x81); break;
+        case 'B': PORTB = 0x40 | (~PINB & 0x81); break;
+        case 'C': PORTB = 0x40 | (~PINB & 0x81); break;
+        case 'D': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '*': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '0': PORTB = 0x40 | (~PINB & 0x81); break;
+        case '#': PORTB = 0x40 | (~PINB & 0x81); break;
         default: PORTB = 0x00; break;
     }
     return state;
@@ -54,7 +54,7 @@ int check_pass(int state) {
     state = pass_wait;
     switch(state) {
         case pass_wait:
-            PORTB = input | PINB;
+            PORTB = (input && (~PINB & 0x01) | (~PINB & 0xFE);
     }
     switch(state) {
 
