671505ce7d26e91d47782f477a554e27e9524fd0
hscottvo <hscottvo@gmail.com>
2021-05-23 13:46:49 -0700

    test scheduler with pad input

--- source/main.c
+++ source/main.c
@@ -20,7 +20,27 @@ enum keypad_states {light_on, light_off};
 
 int keypad_tick(int state) {
     unsigned char pad = GetKeypadKey();
-    PORTB = 0x01;
+    switch(pad) {
+        case '\0': PORTB = 0x1F; break;
+        case '1': PORTB = 0x01; break;
+        case '2': PORTB = 0x01; break;
+        case '3': PORTB = 0x03; break;
+        case '4': PORTB = 0x04; break;
+        case '5': PORTB = 0x05; break;
+        case '6': PORTB = 0x06; break;
+        case '7': PORTB = 0x07; break;
+        case '8': PORTB = 0x08; break;
+        case '9': PORTB = 0x09; break;
+        case 'A': PORTB = 0x0A; break;
+        case 'B': PORTB = 0x0B; break;
+        case 'C': PORTB = 0x0C; break;
+        case 'D': PORTB = 0x0D; break;
+        case '*': PORTB = 0x0E; break;
+        case '0': PORTB = 0x00; break;
+        case '#': PORTB = 0x0F; break;
+        default: PORTB = 0x1B; break;
+    }
+    return state;
 }
 
 int main(void) {
