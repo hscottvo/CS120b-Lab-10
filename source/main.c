fc7cc017e0953aee3c8a6b321cf49292222a238d
hscottvo <hscottvo@gmail.com>
2021-05-23 13:56:43 -0700

    try 8's

--- source/main.c
+++ source/main.c
@@ -20,23 +20,23 @@
 int keypad_tick(int state) {
     unsigned char pad = GetKeypadKey();
     switch(pad) {
-        case '\0': PORTB = 0x1F; SetBit(PORTB, 7, 0); break;
-        case '1': PORTB = 0x01; SetBit(PORTB, 7, 1); break;
-        case '2': PORTB = 0x01; SetBit(PORTB, 7, 1); break;
-        case '3': PORTB = 0x03; SetBit(PORTB, 7, 1); break;
-        case '4': PORTB = 0x04; SetBit(PORTB, 7, 1); break;
-        case '5': PORTB = 0x05; SetBit(PORTB, 7, 1); break;
-        case '6': PORTB = 0x06; SetBit(PORTB, 7, 1); break;
-        case '7': PORTB = 0x07; SetBit(PORTB, 7, 1); break;
-        case '8': PORTB = 0x08; SetBit(PORTB, 7, 1); break;
-        case '9': PORTB = 0x09; SetBit(PORTB, 7, 1); break;
-        case 'A': PORTB = 0x0A; SetBit(PORTB, 7, 1); break;
-        case 'B': PORTB = 0x0B; SetBit(PORTB, 7, 1); break;
-        case 'C': PORTB = 0x0C; SetBit(PORTB, 7, 1); break;
-        case 'D': PORTB = 0x0D; SetBit(PORTB, 7, 1); break;
-        case '*': PORTB = 0x0E; SetBit(PORTB, 7, 1); break;
-        case '0': PORTB = 0x00; SetBit(PORTB, 7, 1); break;
-        case '#': PORTB = 0x0F; SetBit(PORTB, 7, 1); break;
+        case '\0': PORTB = 0x1F; break;
+        case '1': PORTB = 0x81; break;
+        case '2': PORTB = 0x81; break;
+        case '3': PORTB = 0x83; break;
+        case '4': PORTB = 0x84; break;
+        case '5': PORTB = 0x85; break;
+        case '6': PORTB = 0x86; break;
+        case '7': PORTB = 0x87; break;
+        case '8': PORTB = 0x88; break;
+        case '9': PORTB = 0x89; break;
+        case 'A': PORTB = 0x8A; break;
+        case 'B': PORTB = 0x8B; break;
+        case 'C': PORTB = 0x8C; break;
+        case 'D': PORTB = 0x8D; break;
+        case '*': PORTB = 0x8E; break;
+        case '0': PORTB = 0x80; break;
+        case '#': PORTB = 0x8F; break;
         default: PORTB = 0x1B; break;
     }
     return state;
